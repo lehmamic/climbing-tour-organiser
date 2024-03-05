8296d80cec275de35627756dedadc1cba6f93885
Michael Lemann <2562610+lehmamic@users.noreply.github.com>
2024-03-05 19:22:43 +0100

    setup scss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 
-import "./globals.css";
+import "@styles/globals.scss";
 import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
