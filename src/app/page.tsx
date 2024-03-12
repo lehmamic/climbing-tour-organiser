4363a95b813393ae16821011e2a07a77fa44d5aa
Michael Lemann <2562610+lehmamic@users.noreply.github.com>
2024-03-12 13:43:49 +0100

    remove the next js framework

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { NextPage } from 'next';
-
-const HomePage: NextPage = () => {
-  return <main className="container mx-auto flex min-h-screen flex-col"></main>;
-};
-
-export default HomePage;
--- src/model/paged.ts
+++
@@ -1,4 +0,0 @@
-export interface Paged<T> {
-  data: T[];
-  totalCount: number;
-}
